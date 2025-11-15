888dc622f04bbd605321de369793c47eafb177b3
Ben Goddard <goddard.ben04@gmail.com>
2025-11-14 17:09:47 -0700

    Created custom hooks

--- src/hooks/useCoffees.jsx
+++ src/hooks/useCoffees.jsx
@@ -1,10 +1,11 @@
 import { useContext } from "react";
 import { CoffeeContext } from "../pages/CoffeeContext"; // adjust path if needed
 
-export function useCoffees() {
+function useCoffees() {
   const context = useContext(CoffeeContext);
   if (!context) {
     throw new Error("useCoffees must be used within a CoffeeProvider");
   }
   return context;
 }
+export default useCoffees;
