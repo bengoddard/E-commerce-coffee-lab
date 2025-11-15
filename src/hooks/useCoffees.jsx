84fcbe92fedbce7764f5cd009c45908dd60271e9
Ben Goddard <goddard.ben04@gmail.com>
2025-11-14 17:19:25 -0700

    Implemented search funcionality and created custom hooks

--- src/hooks/useCoffees.jsx
+++ src/hooks/useCoffees.jsx
@@ -1,11 +1,10 @@
 import { useContext } from "react";
-import { CoffeeContext } from "../pages/CoffeeContext"; // adjust path if needed
+import { CoffeeContext } from "../pages/CoffeeContext";
 
-function useCoffees() {
+export function useCoffees() {
   const context = useContext(CoffeeContext);
   if (!context) {
     throw new Error("useCoffees must be used within a CoffeeProvider");
   }
   return context;
 }
-export default useCoffees;
